426fb75f599f941a657d0be021b8ad556a98c0c3
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-03-19 22:05:43 +0530

    setting up signup api route and functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,10 +6,12 @@ import { connectDB } from './lib/db.js';
 dotenv.config()
 const app = express();
 
-
+app.use(express.json());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running at port ${PORT}`);
