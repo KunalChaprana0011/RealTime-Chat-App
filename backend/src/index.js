fad9c2446656c44c82d7192784e0a3e1c5b3712e
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-03-22 11:59:41 +0530

    completing updateProfile and other routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,19 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
 import dotenv from "dotenv";
-import { connectDB } from './lib/db.js';
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
-dotenv.config()
+dotenv.config();
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 
-
-
 app.listen(PORT, () => {
-    console.log(`Server is running at port ${PORT}`);
-    connectDB();
-    
+  console.log(`Server is running at port ${PORT}`);
+  connectDB();
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,13 +1,17 @@
-import express from 'express'; 
-import {  signin, signout, signup } from '../controllers/auth.controller.js';
+import express from "express";
+import { signin, signout, signup ,updateProfile,checkAuth} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/protectRoute.middleware.js";
 
 const router = express.Router();
 
+router.post("/signup", signup);
 
-router.post("/signup",signup)
+router.post("/signout", signout);
 
-router.post("/signout",signout)
+router.post("/signin", signin);
 
-router.post("/signin",signin)
+router.put("/update-profile",protectRoute,updateProfile);
+
+router.get("/check",protectRoute,checkAuth)
 
 export default router;
