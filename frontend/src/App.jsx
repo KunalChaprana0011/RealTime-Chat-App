c21dae9c7ccb36403d9b1b11f0290f0283f64fd4
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-03-25 21:52:30 +0530

    setting up frontend routes , files and folders and setting up axios

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,23 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home.jsx"
+import SignUp from "./pages/SignUp.jsx"
+import SignIn from "./pages/SignIn.jsx"
+import Settings from "./pages/Settings.jsx"
+import Profile from "./pages/Profile.jsx"
+import Navbar from "./components/Navbar.jsx"
+
 const App = () => {
   return (
-    <div className="text-red-800 text-2xl">Hello Worlds!!</div>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element = {<Home />} /> 
+        <Route path="/signup" element = {<SignUp />} /> 
+        <Route path="/signin" element = {<SignIn />} /> 
+        <Route path="/settings" element = {<Settings />} /> 
+        <Route path="/profile" element = {<Profile />} /> 
+      </Routes>
+    </div>
   )
 }
 
