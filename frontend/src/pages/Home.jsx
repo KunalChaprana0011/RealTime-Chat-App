9fcbfba837e7be45ff0e320b399836f153087179
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-03-31 11:45:12 +0530

    setting up useChatStore and working on HomePage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,23 @@
+import ChatContainer from "../components/ChatContainer.jsx";
+import NoChatContainer from "../components/NoChatContainer.jsx";
+import Sidebar from "../components/Sidebar.jsx";
+import { useChatStore } from "../zustand/useChatStore";
 
 const Home = () => {
+  const {selectedUser} = useChatStore();
   return (
-    <div>Home</div>
-  )
-}
+    <div className="h-screen bg-base-300">
+      <div className="flex items-center justify-center pt-20 px-4">
+        <div className="bg-base-100 rounded-lg shadow-xl  w-full max-w-7xl h-[calc(100vh-8rem)]">
+          <div className="flex h-full rounded-lg overflow-hidden">
+            <Sidebar />  
+            {!selectedUser ? <NoChatContainer /> : <ChatContainer />}
 
-export default Home
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
