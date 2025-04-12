de866c7626145896402fc7011e5065fe50c2aa18
KunalChaprana0011 <kunalchaprana8093@gmail.com>
2025-04-12 08:20:52 +0530

    final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,12 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 import { app,server } from "./lib/socket.js";
+import path from "path";
 
 dotenv.config();
+
+
+const __dirname = path.resolve();
 
 
 app.use(express.json({ limit: "1mb" }));
@@ -24,6 +28,15 @@ const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
